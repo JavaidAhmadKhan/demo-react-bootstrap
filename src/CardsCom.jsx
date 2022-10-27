248d8256a3d02c3b5ccc3273a14d9fc4f919e48c
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-10-28 00:05:44 +0530

    intialised project with react bootstrap

---
+++ src/CardsCom.jsx
@@ -0,0 +1,112 @@
+import Card from 'react-bootstrap/Card';
+import './cardcontainer.css'
+
+function BasicExample() {
+    return (
+        <div className='cardContainer'>
+            <Card style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='cardContainer' style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+            <Card className='cardContainer' style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+            <Card className='cardContainer' style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+            <Card className='cardContainer' style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+            <Card className='cardContainer' style={{ width: '24rem', margin: "10px" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default BasicExample;
